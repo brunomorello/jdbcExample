be8a7c12e7dd9c24655007548f82698dddce6049
Bruno Moreno <brunomorello7@gmail.com>
2021-02-15 19:01:59 -0300

    using try-with-resources

--- src/main/java/br/com/bmo/dao/TestInsert.java
+++ src/main/java/br/com/bmo/dao/TestInsert.java
@@ -9,44 +9,46 @@ import java.sql.Statement;
 
 public class TestInsert {
 	public static void main(String[] args) throws SQLException {
-		
+
 		ConnectionFactory connFactory = new ConnectionFactory();
-		Connection conn = connFactory.getConnection();
-		conn.setAutoCommit(false);
-		
-		try {
+		// try-with-resources
+		try (Connection conn = connFactory.getConnection()) {
+
+			conn.setAutoCommit(false);
 			String sql = "INSERT INTO product (name, description, price) VALUES (?, ?, ?)";
-			PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-			
-			setSqlValues("KEYBOARD", "bluetooth wireless", BigDecimal.valueOf(50.0), stmt);
-			setSqlValues("DISPLAY", "lg display", BigDecimal.valueOf(400.0), stmt);
-			
-			conn.commit();
-			conn.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTED");
-			conn.rollback();
-			conn.close();
+
+			try (PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
+
+				setSqlValues("KEYBOARD", "bluetooth wireless", BigDecimal.valueOf(50.0), stmt);
+				setSqlValues("DISPLAY", "lg display", BigDecimal.valueOf(400.0), stmt);
+
+				conn.commit();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTED");
+				conn.rollback();
+			} 
 		}
 	}
 
-	private static void setSqlValues(String name, String description, BigDecimal price, PreparedStatement stmt) throws SQLException {
+	private static void setSqlValues(String name, String description, BigDecimal price, PreparedStatement stmt)
+			throws SQLException {
 		stmt.setString(1, name);
 		stmt.setString(2, description);
 		stmt.setBigDecimal(3, price);
-		
+
 		if (name.equals("DISPLAY")) {
 			throw new RuntimeException("Display cannot be inserted");
 		}
-		
+
 		stmt.execute();
-		
-		ResultSet rst = stmt.getGeneratedKeys();
-		
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("product created - id: " + id);
+
+		try (ResultSet rst = stmt.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("product created - id: " + id);
+			}			
 		}
+
 	}
 }
